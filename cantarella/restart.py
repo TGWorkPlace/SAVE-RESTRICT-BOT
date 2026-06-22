fa287b5f4a0c1f1335a13707d4eabd8e6bffa9d3
TGWorkPlace <DramaShip.org@gmail.com>
2026-06-22 11:28:12 +0530

    Update restart.py

--- cantarella/restart.py
+++ cantarella/restart.py
@@ -1,7 +1,15 @@
 import os
 import sys
 import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.errors import (
+    FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant,
+    InviteHashExpired, UsernameNotOccupied, AuthKeyUnregistered, UserDeactivated, UserDeactivatedBan
+)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery, InputMediaPhoto
 from config import API_ID, API_HASH, ERROR_MESSAGE, ADMINS
+import math
+from logger import LOGGER
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def stop_button(bot, message):
